f30133b25fe28e8717417aa9471705637900f1a4
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-06-10 23:06:12 +0900

    product 스키마 모델링 productSchema.js 에 스키마 작성 후 models/ 에서 모델링 작업

--- models/index.js
+++ models/index.js
@@ -1,1 +1,5 @@
 const mongoose = require('mongoose');
+const productSchema = require('./schemas/productSchema');
+
+// ProductSchema 모델링
+exports.Product = mongoose.model('Product', productSchema);
--- models/schemas/productSchema.js
+++ models/schemas/productSchema.js
@@ -1,4 +1,98 @@
 const {Schema} = require('mongoose');
-const {nanoid} = require('nanoid');
-const newDate = require('../../utils/newDate');
 // 중복 없는 문자열을 생성해주는 nanoid
+const {nanoid} = require('nanoid');
+// 상품 또는 상품평이 추가 또는 수정될 때마다 날짜 데이터를 만들어주는 newDate()
+const newDate = require('../../utils/newDate');
+
+const commentSchema = new Schema({
+    // 상품평 내용
+    content: String,
+    // 상품 한줄평 작성자
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    // 작성 시간
+    createAt: {
+        type: String,
+        default: () => { return newDate() },
+        require: true
+    },
+    // 수정 시간
+    updateAt: {
+        type: String,
+        default: () => { return newDate() },
+        require: true
+    },
+    // 좋아요 (true: 좋아요, false: 아쉬워요)
+    like: {
+        type: Boolean,
+        require: true
+    }
+}, {
+    timestamps: true
+});
+
+const productSchema = new Schema({
+    // 상품 고유 번호
+    __id: {
+        type: String,
+        default: () => { return nanoid() },
+        require: true,
+        index: true
+    },
+    // 가격
+    price: {
+        type: String,
+        require: true
+    },
+    // 상품 이미지
+    image: {
+        type: String,
+        require: true
+    },
+    // 배송 방법
+    delivery: {
+        type: String,
+        require: true
+    },
+    // 상품 이름
+    title: {
+        type: String,
+        required: true
+    },
+    // 상품 광고 문구
+    ad: {
+        type: String,
+        required: true
+    },
+    // 판매자
+    seller: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    // 상품 등록 시간
+    createAt: {
+        type: String,
+        default: () => { return newDate() },
+        require: true
+    },
+    // 상품 업데이트 시간
+    updateAt: {
+        type: String,
+        default: () => { return newDate() },
+        require: true
+    },
+    // 상품 상세 정보
+    detailContent: {
+        type: String,
+        require: true
+    },
+    // 상품 한줄평
+    comments: [commentSchema]
+},{
+    timestamps: true
+});
+
+module.exports = productSchema;
