62707a15a97b0b6d767064e262948a35860df034
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-06-12 13:03:44 +0900

    Feat: Product CRUD 라우터 서비스 작성 postman 동작 확인

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,17 +6,38 @@ const router = Router();
 
 // create
 router.post('/', asyncHandler(async (req, res) => {
-    const {price, image, detailImage, delivery, title, ad, maker} = req.body;
-    const result = await productService.createProduct({price, image, detailImage, delivery, title, ad, maker});
+    const query = req.body;
+    const result = await productService.createProduct(query);
     res.json(result);
 }));
+
 // find all
+router.get('/', asyncHandler(async (req, res) => {
+    const result = await productService.findAllProduct();
+    res.json(result);
+}));
 
 // findOne
+router.get('/:__id', asyncHandler(async (req, res) => {
+    const {__id} = req.params;
+    const result = await productService.findProduct({__id});
+    res.json(result);
+}));
 
 // update
+router.post('/update/:__id', asyncHandler(async (req, res) => {
+    const {__id} = req.params;
+    const query = req.body;
+    const result = await productService.findUpdate({__id}, query);
+    res.json(result);
+}));
 
 // delete
+router.delete('/delete/:__id', asyncHandler(async (req,res) => {
+    const {__id} = req.params;
+    const result = await productService.findDelete({__id});
+    res.json(result);
+}));
 
 
 module.exports = router;
