c60b58e6937d2a4f32c9505163ed42cb58678985
hyunjin <drlee668@gmail.com>
2024-06-17 11:08:06 +0900

    로긴체크, 리퀘스트유저체크 에러처리변경

--- middlewares/reqUserCheck.js
+++ middlewares/reqUserCheck.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
     if(!req.user){
-        throw new Error("로그인하지 않은 사용자입니다.");
+        const error = new Error();
+        Object.assign(error, {code: 400, message: "로그인하지 않은 사용자입니다."})
+        throw Error;
     }
     next();
 }
+
