f7bdcd6d85b0f6984d49c585e6cfdf5491ff5599
[SW9] 형건우 <gudrjsdn8825@naver.com>
2024-06-17 11:43:55 +0900

    Refactor: order,product,user Service 에서 발생하는 error 처리를 error 객체에 code, message 를 담아서 throw 로 변경 기존 : throw new Error, 변경 : throw error(new Error)

--- services/productService.js
+++ services/productService.js
@@ -50,7 +50,9 @@ class ProductService {
     async findById({nanoid}){
         const product = await Product.findOne({nanoid}).populate('category');
         if(!product) {
-            throw new Error("조회된 상품이 없습니다.");
+            const error = new Error();
+            Object.assign(error, {code: 404, message: "조회된 상품이 없습니다."})
+            throw error;
         } else {
             // 후기 내용 read 추가
             await Product.populate(product.comments, {
@@ -69,18 +71,24 @@ class ProductService {
     async createComment({nanoid}, reqUser, bodyData){
         const product = await Product.findOne({nanoid});
         if(!product){
-            throw new Error("조회된 상품이 없습니다.");
+            const error = new Error();
+            Object.assign(error, {code: 404, message: "조회된 상품이 없습니다."})
+            throw error;
         } else {
             const author = await User.findOne({
                 nanoid: reqUser.nanoid
             });
             if(!bodyData.content) {
-                throw new Error("후기 내용을 작성해주세요.");
+                const error = new Error();
+                Object.assign(error, {code: 400, message: "후기 내용을 작성해주세요."})
+                throw error;
             }
             // 한 상품에 후기를 한 개로 제한
             product.comments.forEach(v => {
                 if(v.author.toString() === author._id.toString()){
-                    throw new Error("한 상품에 한 개의 후기만 작성할 수 있습니다.");
+                    const error = new Error();
+                    Object.assign(error, {code: 400, message: "한 상품에 한 개의 후기만 작성할 수 있습니다."})
+                    throw error;
                 }
             })
             // $push 오퍼레이터 : 상품에 추가되는 후기 요청 처리
@@ -102,7 +110,9 @@ class ProductService {
     async updateById({nanoid}, bodyData){
         const product = await Product.findOne({nanoid});
         if(!product){
-            throw new Error("조회된 상품이 없습니다.");
+            const error = new Error();
+            Object.assign(error, {code: 404, message: "조회된 상품이 없습니다."})
+            throw error;
         } else {
             await Product.updateOne(product, bodyData);
             return {message: `${nanoid} 상품 수정 완료`};
@@ -113,13 +123,17 @@ class ProductService {
     async updateCommentById({nanoid}, reqUser, bodyData){
         const product = await Product.findOne({nanoid});
         if(!product){
-            throw new Error("조회된 상품이 없습니다.");
+            const error = new Error();
+            Object.assign(error, {code: 404, message: "조회된 상품이 없습니다."})
+            throw error;
         } else {
             const author = await User.findOne({
                 nanoid: reqUser.nanoid
             });
             if(!bodyData.content) {
-                throw new Error("후기 내용을 작성해주세요.");
+                const error = new Error();
+                Object.assign(error, {code: 400, message: "후기 내용을 작성해주세요."})
+                throw error;
             }
             // 내가 작성한 후기가 없을 때 에러처리
             const data = product.comments.find(v => {
@@ -127,7 +141,9 @@ class ProductService {
             });
 
             if(!data){
-                throw new Error("해당 상품에 작성하신 후기가 없습니다.");
+                const error = new Error();
+                Object.assign(error, {code: 404, message: "해당 상품에 작성하신 후기가 없습니다."})
+                throw error;
             }
             // $set 오퍼레이터 : 상품에 수정되는 후기 요청 처리
             const comment = await Product.findOneAndUpdate(
@@ -146,7 +162,9 @@ class ProductService {
     async deleteById({nanoid}) {
         const product = await Product.findOne({nanoid});
         if(!product){
-            throw new Error("조회된 상품이 없습니다.");
+            const error = new Error();
+            Object.assign(error, {code: 404, message: "조회된 상품이 없습니다."})
+            throw error;
         } else {
             await Product.deleteOne(product);
             return {message: `${nanoid} 상품 삭제 완료`};
@@ -157,7 +175,9 @@ class ProductService {
     async deleteCommentById({nanoid}, reqUser){
         const product = await Product.findOne({nanoid});
         if(!product){
-            throw new Error("조회된 상품이 없습니다.");
+            const error = new Error();
+            Object.assign(error, {code: 404, message: "조회된 상품이 없습니다."})
+            throw error;
         } else {
             const author = await User.findOne({
                 nanoid: reqUser.nanoid
@@ -167,7 +187,9 @@ class ProductService {
                 return v.author.toString() === author._id.toString();
             });
             if(!data){
-                throw new Error("해당 상품에 작성하신 후기가 없습니다.");
+                const error = new Error();
+                Object.assign(error, {code: 404, message: "해당 상품에 작성하신 후기가 없습니다."})
+                throw error;
             }
 
             // $pull 오퍼레이터 : 상품에 제거되는 후기 요청 처리
