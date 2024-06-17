ecbce805f2ea3e8d8802e1e2fb982b8ef1da16c7
user <youngelisha@naver.com>
2024-06-17 11:31:51 +0900

    jyfeed2

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
     if(!req.user.is_admin){
-        next(err);
+        const error = new Error();
+        Object.assign(error, {code: 401, message: "접근할 수 없는 요청입니다."})
+        throw error;
     }
+    next();
 }
