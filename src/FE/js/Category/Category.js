1ee22c36341adc3d237eafd746b783e63b744e37
lim12749 <50003828+lim12749@users.noreply.github.com>
2024-06-21 07:39:22 +0900

    로컬스토리지 중복데이터 삭제

--- src/FE/js/Category/Category.js
+++ src/FE/js/Category/Category.js
@@ -8,18 +8,20 @@ ViewProductPage.forEach(product => {
     });
 });
 
-// 담기 버튼 클릭
+let quantity = 0; // 모달의 수량을 저장하는 변수
 document.addEventListener("DOMContentLoaded", () => {
-    let quantity = 1; // 모달의 수량을 저장하는 변수
-    
     // 모달 열기 버튼 이벤트 리스너 설정
+    initializeCartCounter(); //
+    console.log("페이지 오픈");
     document.querySelectorAll(".Cart-Btns").forEach( button => {
-        button.addEventListener('click', (event) => {
+        button.addEventListener('click', (event) => 
+        {
+            //이벤트 전달 안되게 뒤에 a링크하고 버튼이 겹쳐서 이벤트가 발생하는걸 막음
             event.preventDefault();
-            event.stopPropagation(); //이벤트 전달 안되게 하기
+            event.stopPropagation(); 
 
             const findElement = button.closest('.swiper-slide');
-            //if (!findElement) return;
+                if (!findElement) return;
             
             //가지고있는 아이템 연결
             const product = {
@@ -30,58 +32,55 @@ document.addEventListener("DOMContentLoaded", () => {
                 image: findElement.querySelector('.Goods-Image img').src.split('/').pop()
             };
 
-            showModal(product);//링크 열기
+            showModal(product); //모달 오픈
         });
     });
-
-        // 모달 열기 및 제품 정보 설정 함수
-        function showModal(product) 
-        {
-
-            const modal = document.getElementById('CartModal');
-            modal.classList.remove('hidden');
-            modal.classList.add('show');
-
-            // 모달에 제품 정보 설정
-            document.getElementById('ModalProductName').textContent = product.title;
-            document.getElementById('ModalProductPrice').textContent = product.price;
-            document.getElementById('ModalProductDiscountedPrice').textContent = product.discountedPrice;
-            document.getElementById('ModalproductSale').textContent = product.discount;
-            document.getElementById('ModalProductImage').src = `../img/TextImage/${product.image}`;
-
-            // 초기 수량 설정
-            quantity = 1;
-            const productQuantityElement = document.getElementById('productQuantity');
-            const decreaseQtyBtn = document.getElementById('decreaseQtyBtn');
-            productQuantityElement.textContent = quantity;
-            decreaseQtyBtn.disabled = true;
-
-            document.getElementById('increaseQtyBtn').addEventListener('click', () => {
-                quantity++;
-                productQuantityElement.textContent = quantity;
-                decreaseQtyBtn.disabled = false;
-            });
-
-            decreaseQtyBtn.addEventListener('click', () => {
-                if (quantity > 1) {
-                    quantity--;
-                    productQuantityElement.textContent = quantity;
-                    if (quantity === 1) {
-                        decreaseQtyBtn.disabled = true;
-                    }
-                }
-            });
-
-            // 일정 시간 후에 모달을 자동으로 닫기
-            setTimeout(() => {
-                modal.classList.remove('show');
-                modal.classList.add('hidden');
-            }, CLOSETIME);
-        }
-        
-    // 초기 카트 카운터 설정
-    initializeCartCounter();
 });
+
+function showModal(product)
+{
+    const modal = document.getElementById('CartModal');
+    modal.classList.remove('hidden'); //히든으로 모달을 숨겨놨는데 class리스트를 제거해서 시각화함
+    modal.classList.add('show');
+
+    // 모달에 제품 정보 설정
+    document.getElementById('ModalProductName').textContent = product.title;
+    document.getElementById('ModalProductPrice').textContent = product.price;
+    document.getElementById('ModalProductDiscountedPrice').textContent = product.discountedPrice;
+    document.getElementById('ModalproductSale').textContent = product.discount;
+    document.getElementById('ModalProductImage').src = `../img/TextImage/${product.image}`;
+
+    // 초기 수량 설정
+    quantity = 1;
+    const productQuantityElement = document.getElementById('productQuantity');
+    const decreaseQtyBtn = document.getElementById('decreaseQtyBtn'); //수량감소
+    productQuantityElement.textContent = quantity;
+    decreaseQtyBtn.disabled = true; //수량이 1이라서 비활성화
+
+    //수량증가 로직
+    document.getElementById('increaseQtyBtn').addEventListener('click', () => {
+        quantity++;
+        productQuantityElement.textContent = quantity;
+        decreaseQtyBtn.disabled = false;
+    });
+    //수량 감소 로직
+    decreaseQtyBtn.addEventListener('click', () => {
+        if (quantity > 1)
+        {
+            quantity--;
+            productQuantityElement.textContent = quantity;//텍스트 표시
+            if (quantity === 1) {
+                decreaseQtyBtn.disabled = true;
+            }
+        }
+    });
+
+    // 일정 시간 후에 모달을 자동으로 닫기
+    setTimeout(() => {
+        modal.classList.remove('show');
+        modal.classList.add('hidden');
+    }, CLOSETIME);
+}
 
 /*
  * 페이지가 새로고침되거나 처음 오픈될 때
